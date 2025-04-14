6445fbccfe49e9495967263191d1fcdcde509ad2
Julatl05 <22301061550050@cetis155.edu.mx>
2025-04-14 07:34:28 -0600

    cambios iniciales hermano (adminhome)

--- app/src/main/java/com/example/herclass/AdminHome.java
+++ app/src/main/java/com/example/herclass/AdminHome.java
@@ -1,12 +1,16 @@
 package com.example.herclass;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminHome extends AppCompatActivity {
 
@@ -15,10 +19,11 @@ public class AdminHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+
+    public void Regreso(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }

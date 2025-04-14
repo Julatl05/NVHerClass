71125ee09f72a8c8876ba43b16b014fe945f06cf
fabianelyorch <trejofabian114@gmail.com>
2025-04-14 09:12:47 -0600

    admon home para buscar usuarios por id

--- app/src/main/java/com/example/herclass/AdminHome.java
+++ app/src/main/java/com/example/herclass/AdminHome.java
@@ -7,9 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminHome extends AppCompatActivity {
-
+    EditText etMain, etPassword, etEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,31 @@ public class AdminHome extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        setContentView(R.layout.activity_admin_home);
+        etMain = findViewById(R.id.etMain);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
+
+        etMain.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Tampoco aquí
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                String input = s.toString();
+
+                // Llenamos los otros campos automáticamente
+                etPassword.setText(input + "123");
+                etEmail.setText(input + "@email.com");
+            }
         });
     }
 }
